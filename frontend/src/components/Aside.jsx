ae3e25f930b5c69159bb2d4db4b2bb7423132d87
Lupusdog <gc.jp1130@gmail.com>
2022-08-18 20:25:39 +0900

    init

--- frontend/src/components/Aside.jsx
+++ frontend/src/components/Aside.jsx
@@ -5,12 +5,20 @@ import { useEffect } from "react";
 
 export const Aside = memo ((props) => {
 
+  const [deltaTime, setTime] = useState(0);
   const [Comment, setComment] = useState("");
   const [Chat, setChat] = useState([]);
 
   useEffect(() => {
     fetch("/chat").then(res => res.json()).then(data => setChat(data.chat))
   })
+
+  setInterval((args) => {
+    if(deltaTime === 500){
+      fetch("/chat").then(res => res.json()).then(data => setChat(data.chat))
+      setTime(0);
+    }
+  },1)
 
   return(
     <Box  w="25%" h="450px" padding="50px">
@@ -29,6 +37,7 @@ export const Aside = memo ((props) => {
                 comment: Comment
               })
           }).then((res) => res.json()).then((data) => setChat(data.chat))
+          setTime(0);
         }}>送信</Button>
     </Box>
   )
